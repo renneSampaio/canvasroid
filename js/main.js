7ec2c7e0ad316a66f5b5af8668fa7751581d3d8d
Renne Sampaio <rennesampaio97@gmail.com>
2017-05-26 19:47:39 -0300

    Fix background opacity and Asteroid velocity

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var numAsteroids = 10;
+var numAsteroids = 30;
 var asteroidField = [];
 
 for (var i = 0; i < numAsteroids; i++) {
     var pos = [(Math.random() * canvas.width), (Math.random() * canvas.height)];
-    var vel = [(Math.random() * 10) + 10, (Math.random() * 10) + 10];
+    var vel = [(Math.random() * 5) + 5, (Math.random() * 5) + 5];
     var radius = (Math.random() * 50) + 20;
     var numVertices = (Math.random() * 8) + 5;
     var maxVariance = (Math.random() * 30) + 20;
@@ -18,7 +18,7 @@ for (var i = 0; i < numAsteroids; i++) {
 
 function updateAndDrawField() {
 
-    ctx.fillStyle = "rgba(20,20,20,20)";
+    ctx.fillStyle = "rgba(0, 0, 0,.4)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     asteroidField.forEach(function (ast) {
         ast.update();
